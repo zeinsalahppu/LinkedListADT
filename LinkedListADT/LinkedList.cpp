68816b6a67778e844a246258951a4c5451fc99e1
zeinsalahppu <zeinsalah@gmail.com>
2020-11-01 12:08:31 +0200

    defined basic class for singly linked list

---
+++ LinkedListADT/LinkedList.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+
+class SinglyLinkedList
+{
+  public:
+    SinglyLinkedList();
+    ~SinglyLinkedList();
+};
+
+
+SinglyLinkedList::SinglyLinkedList()
+{
+
+}
+
+
+SinglyLinkedList::~SinglyLinkedList()
+{
+
+}
+
+
+
+
+void main()
+{
+  cout << "Hello! This is an example program for studying linked lists" << endl;
+  SinglyLinkedList list;
+
+
+  system("PAUSE");
+}
+
